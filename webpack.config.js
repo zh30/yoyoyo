34e1970c1943ad6e9ec37e3ce08456843a38c194
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-25 02:52:31 +0000

    Deploy hertzZhang/yoyoyo to github.com/hertzZhang/yoyoyo.git:gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -16,18 +16,16 @@ module.exports = {
 
   resolve: {
     // Add '.ts' and '.tsx' as resolvable extensions.
-    extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+    extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
   },
 
   module: {
-    loaders: [
-      // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
-      { test: /\.tsx?$/, loader: "ts-loader" }
-    ],
+    rules: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
 
-    preLoaders: [
       // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-      { test: /\.js$/, loader: "source-map-loader" }
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
     ]
   }
 
