69f49afc380c88ec001d91e5b37a95646a22a5bd
hertz <zhanghelook@outlook.com>
2019-05-22 23:42:28 +0800

    feat(typescript): 增加入场动画

--- assets/ts/app.ts
+++ assets/ts/app.ts
@@ -1,21 +1,10 @@
-import { Observable } from "rxjs";
+import { fromEvent } from 'rxjs';
 
-var observable = Observable.create((observer: any) => {
-  observer.next("Hello World!");
-  observer.next("Hello Again!");
-  observer.complete();
-  observer.next("Bye");
-});
-
-observable.subscribe(
-  (x: any) => logItem(x),
-  (error: any) => logItem("Error: " + error),
-  () => logItem("Completed")
-);
-
-function logItem(val: any) {
-  var node = document.createElement("li");
-  var textnode = document.createTextNode(val);
-  node.appendChild(textnode);
-  document.body.appendChild(node);
+const bodyEvent = fromEvent(window, 'load');
+const bodyAddClass = (className: string) => {
+  const body = document.body;
+  body.classList.add(className);
 }
+bodyEvent.subscribe(
+  () => bodyAddClass('render')
+);
