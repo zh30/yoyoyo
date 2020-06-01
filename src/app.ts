653a2d53c2166274df349daa4cddf1460712def7
ZhangHe <zhanghecool@outlook.com>
2020-06-01 19:04:53 +0800

    feat(init): 初始化webpack

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import { fromEvent } from "rxjs";
 import init from "./sw";
 
-init();
-
 const bodyEvent = fromEvent(document, "DOMContentLoaded");
 const bodyAddClass = (className: string) => {
   const body = document.body;
   body.classList.add(className);
 };
-bodyEvent.subscribe(() => bodyAddClass("render"));;
+
+bodyEvent.subscribe(() => bodyAddClass("render"));
+init();
