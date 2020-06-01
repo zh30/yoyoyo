2e1beeae2b05025bba1280fbee28e1f006baf82c
zhanghecool <zhanghecool@users.noreply.github.com>
2020-06-01 07:55:10 +0000

    Automated deployment: Mon Jun  1 07:55:10 UTC 2020 487661083f4ddf1c589a6a07c7de521cf78cccdc

--- assets/ts/app.ts
+++ assets/ts/app.ts
@@ -8,4 +8,21 @@ const bodyAddClass = (className: string) => {
   const body = document.body;
   body.classList.add(className);
 };
-bodyEvent.subscribe(() => bodyAddClass("render"));;
+bodyEvent.subscribe(() => bodyAddClass("render"));
+
+let deferredPrompt: Event;
+
+window.addEventListener("beforeinstallprompt", (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can install the PWA
+  // showInstallPromotion();
+});
+
+const btn = document.getElementById("installBtn");
+btn.addEventListener("click", (e) => {
+  console.info("e: ", e);
+  console.info("deferredPrompt: ", deferredPrompt);
+});
